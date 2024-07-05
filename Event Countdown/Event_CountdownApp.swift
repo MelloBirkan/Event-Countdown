0ca05baeeeefdf42a456daba14c756dac5eaeb4f
Marcello Gonzatto Birkan <mgbirkan@icloud.com>
2024-07-04 23:37:45 -0300

    Initial Commit

---
+++ Event Countdown/Event_CountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  Event_CountdownApp.swift
+//  Event Countdown
+//
+//  Created by Marcello Gonzatto Birkan on 04/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Event_CountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
